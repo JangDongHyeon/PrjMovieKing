b760d0e75f99b02db4a904e28f91a339de2771cb
Sungmin Kim <floor3th@gmail.com>
2018-12-30 13:28:34 +0900

    revert
    revert

--- MovieKing/src/main/java/com/spring/movieKing/user/controller/GoogleController.java
+++ MovieKing/src/main/java/com/spring/movieKing/user/controller/GoogleController.java
@@ -1,9 +1,36 @@
 package com.spring.movieKing.user.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.google.api.Google;
+import org.springframework.social.google.api.impl.GoogleTemplate;
+import org.springframework.social.google.api.plus.Person;
+import org.springframework.social.google.api.plus.PlusOperations;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.GrantType;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/google/*")
 public class GoogleController {
-	/* GoogleLogin 
+	/* GoogleLogin */
 	@Autowired
 	private GoogleConnectionFactory googleConnectionFactory;
 	@Autowired
@@ -13,7 +40,7 @@ public class GoogleController {
 	@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
 	public String login(Model model, HttpSession session) {
 
-		 구글code 발행 
+		/* 구글code 발행 */
 		OAuth2Operations oauthOperations = googleConnectionFactory.getOAuthOperations();
 		String url = oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, googleOAuth2Parameters);
 
@@ -21,7 +48,7 @@ public class GoogleController {
 
 		model.addAttribute("google_url", url);
 
-		 생성한 인증 URL을 View로 전달 
+		/* 생성한 인증 URL을 View로 전달 */
 		return "login";
 	}
 
@@ -31,5 +58,5 @@ public class GoogleController {
 		System.out.println("여기는 googleCallback");
 
 		return "googleSuccess";
-	}*/
+	}
 }
